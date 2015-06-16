2e4ec853a6c1ece7cf22750ff0ca3cc703231252
Jan Hoffmann <jhq1@gmx.de>
2015-06-16 03:17:15 +0200

    min und max durch inline functions ersetzt

--- ex/ex08/8.2.c
+++ ex/ex08/8.2.c
@@ -29,8 +29,8 @@ int min ( int a , int b ) {
 }
 
 int minOfMax ( int a , int b , int c , int d ) {
- int tmp1 = max ( a , b );
- int tmp2 = max ( c , d );
- int tmp3 = min ( tmp1 , tmp2 );
+ int tmp1 = a > b ? a : b;
+ int tmp2 = c > d ? c : d;
+ int tmp3 = tmp1 < tmp2 ? tmp1 : tmp2;
  return tmp3;
  }
