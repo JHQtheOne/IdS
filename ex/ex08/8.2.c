aedc2e0fcc8d30e0f46a84c17ef49eeb03dc4ddb
Jan Hoffmann <jhq1@gmx.de>
2015-06-16 02:43:58 +0200

    compilable c file with I/O

--- ex/ex08/8.2.c
+++ ex/ex08/8.2.c
@@ -1,3 +1,19 @@
+#include<stdio.h>
+
+int main(void) {
+	int a, b, c, d;
+	printf("please enter a:");
+	scanf("%d", &a);
+	printf("please enter b:");
+	scanf("%d", &b);
+	printf("please enter c:");
+	scanf("%d", &c);
+	printf("please enter d:");
+	scanf("%d", &d);
+	printf("minOfMax(%d, %d, %d, %d) returned %d", a, b, c, d, minOfMax(a, b, c, d));
+    return 0;
+}
+
 int max ( int a , int b ) {
 	if ( a > b )
 		return a;
@@ -12,9 +28,9 @@ int min ( int a , int b ) {
 		return b;
 }
 
- int minOfMax ( int a , int b , int c , int d ) {
-	int tmp1 = max ( a , b );
-	int tmp2 = max ( c , d );
-	int tmp3 = min ( tmp1 , tmp2 );
-	return tmp3;
+int minOfMax ( int a , int b , int c , int d ) {
+ int tmp1 = max ( a , b );
+ int tmp2 = max ( c , d );
+ int tmp3 = min ( tmp1 , tmp2 );
+ return tmp3;
  }
